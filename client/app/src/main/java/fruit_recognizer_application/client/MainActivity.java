a3f12b731c7d60968d8354dd18f35037f37cb09e
Slavik Kovbasa <slavikovbasa@gmail.com>
2018-11-05 19:49:39 +0200

    added app version from git to be available in code

--- client/app/src/main/java/fruit_recognizer_application/client/MainActivity.java
+++ client/app/src/main/java/fruit_recognizer_application/client/MainActivity.java
@@ -7,6 +7,7 @@ import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import java.io.FileNotFoundException;
@@ -22,6 +23,9 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        TextView versionView = findViewById(R.id.versionView);
+        versionView.setText(BuildConfig.APP_VERSION);
         setContentView(R.layout.activity_main);
     }
 
